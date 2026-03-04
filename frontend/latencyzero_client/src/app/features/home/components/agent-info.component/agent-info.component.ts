c40183c9396e852ab29016b100752f193f81235b
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2026-03-04 18:12:42 +0100

    feat: information about the agent

---
+++ frontend/latencyzero_client/src/app/features/home/components/agent-info.component/agent-info.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-info',
+  imports: [],
+  templateUrl: './agent-info.component.html',
+  styleUrl: './agent-info.component.css',
+})
+export class AgentInfoComponent {
+
+}
--- frontend/latencyzero_client/src/app/features/home/pages/home-page/home-page.ts
+++ frontend/latencyzero_client/src/app/features/home/pages/home-page/home-page.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { GameComponent } from '../../components/game.component/game.component';
 import { AboutUsComponent } from "../../components/about-us.component/about-us.component";
+import { AgentInfoComponent } from "../../components/agent-info.component/agent-info.component";
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterLink, CommonModule, GameComponent, AboutUsComponent],
+  imports: [RouterLink, CommonModule, GameComponent, AboutUsComponent, AgentInfoComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
