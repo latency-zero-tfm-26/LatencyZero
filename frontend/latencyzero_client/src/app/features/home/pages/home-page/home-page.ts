6b798feb053c2e029a10a9e63c3706257560df13
Alejandro-BR <barrionuevorosadoalejandro@gmail.com>
2026-03-04 16:46:36 +0100

    feat: carousel of games at home plate

--- frontend/latencyzero_client/src/app/features/home/pages/home-page/home-page.ts
+++ frontend/latencyzero_client/src/app/features/home/pages/home-page/home-page.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { GameComponent } from '../../components/game.component/game.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, GameComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
